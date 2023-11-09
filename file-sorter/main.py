ee606a68b210cb888a56b5d2171c06b198552333
unknown <dominik.majdandzic@gmail.com>
2023-11-09 09:09:00 +0100

    added console input for source and destination directories

--- file-sorter/main.py
+++ file-sorter/main.py
@@ -1,15 +1,21 @@
 import os
 import shutil
 
-desktop_dir = os.path.join(os.environ['USERPROFILE'], 'Desktop')
-src_dir = "E:\\Downloads"
-dst_dir = "E:\\SORTED_FILES"
-
 # (name of folder, associated extensions)
 sorted_dirs_info = (("image", (".ai", ".gif", ".jpg", ".jpeg", ".bmp", ".svg", ".ico", ".png", ".svg", "webp")),
                     ("audio", (".mp3", ".flac", ".m4a", ".wav")),
                     ("video", (".mp4", ".avi", ".m4v", ".mkv", ".mov", ".h264")),
                     ("text", (".txt", ".doc", ".docx", ".pdf")))
+
+src_dir = ""
+dst_dir = ""
+
+while not os.path.exists(src_dir):
+    src_dir = input("Enter source directory: ")
+
+while not os.path.exists(dst_dir):
+    dst_dir = input("Enter destination directory: ")
+
 
 if not os.path.exists(dst_dir):
     os.mkdir(dst_dir)
@@ -29,5 +35,10 @@ def move_files(src, dst, extensions):
             shutil.move(src_path, dst_path)
 
 
-for dir_info in sorted_dirs_info:
-    move_files(desktop_dir, dst_dir + "\\" + dir_info[0], dir_info[1])
+try:
+    for dir_info in sorted_dirs_info:
+        move_files(src_dir, dst_dir + "\\" + dir_info[0], dir_info[1])
+except Exception:
+    print("Unable to move files")
+else:
+    print("Files move successfully")
